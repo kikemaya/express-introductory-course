ea534d5d461fc76331a6e11d7f9dfcd00cc081c1
kikemaya <enrique.mg.cd@gmail.com>
2024-05-10 15:31:40 -0600

    async code with axios

--- template-engines/src/routes/home.js
+++ template-engines/src/routes/home.js
@@ -1,27 +1,28 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const router = Router();
 
 router.get("/", (request, response) => {
-  let isActive = true
-  
+  let isActive = true;
+
   const users = [
     {
       id: 1,
       name: "John",
-      lastname: "Doe"
+      lastname: "Doe",
     },
     {
       id: 2,
       name: "Ryan",
-      lastname: "Perez"
+      lastname: "Perez",
     },
     {
       id: 3,
       name: "Joe",
-      lastname: "McMillan"
+      lastname: "McMillan",
     },
-  ]
+  ];
 
   response.render("index", { title: "Index page", isActive, users });
 });
@@ -33,6 +34,14 @@ router.get("/about", (request, response) => {
 
 router.get("/dashboard", (request, response) => {
   response.render("dashboard");
+});
+
+router.get("/posts", async (request, response) => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+  response.render("posts", {
+    posts: res.data
+  });
 });
 
 module.exports = router;
